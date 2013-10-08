cc331a8104b1d112c87d37b37e099635f8307102
tao-lu <tao.lu@dena.com>
2013-10-08 18:59:51 +0900

    add skeleton

---
+++ skeleton/test/test_NAME.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+
+class MyUnitTests < Test::Unit::TestCase
+    def setup
+        puts "setup!"
+    end
+
+    def teardown
+        puts "teardown!"
+    end
+
+    def test_basic
+        puts "I RAN!"
+    end
+end
